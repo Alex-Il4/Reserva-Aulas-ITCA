179a545868df60a5851b65b80aadd26b8a9afcba
Alex Jimenez <alexjimenez.az46@gmail.com>
2025-10-19 12:24:42 -0600

    Se implemento herencia de Reserva en ReservaClase tambien se aplico una de las reglas

--- Reservasitca/src/main/java/com/mycompany/reservasitca/models/ReservaClase.java
+++ Reservasitca/src/main/java/com/mycompany/reservasitca/models/ReservaClase.java
@@ -3,11 +3,47 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.reservasitca.models;
+import exceptions.ReglaReservaException;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 /**
  *
  * @author alex
  */
-public class ReservaClase {
+
+// Implementa herencia de Reserva
+public class ReservaClase extends Reserva implements Validable {
+
+    public ReservaClase(String id, Aula aula, LocalDate fecha, LocalTime horaInicio, LocalTime horaFin, String responsable) {
+        super(id, aula, fecha, horaInicio, horaFin, responsable);
+    }
     
+    public ReservaClase(String id, Aula aula, LocalDate fecha, LocalTime horaInicio, LocalTime horaFin, String responsable, EstadoReserva estado) {
+        super(id, aula, fecha, horaInicio, horaFin, responsable, estado);
+    }
+
+    @Override
+    public void validar() throws ReglaReservaException {
+        // Regla específica: solo en aulas TEORICA o LABORATORIO
+        TipoAula tipo = this.getAula().getTipo();
+        if (tipo != TipoAula.TEORICA && tipo != TipoAula.LABORATORIO) {
+            throw new ReglaReservaException("Una Reserva de Clase solo puede realizarse en aulas TEORICA o LABORATORIO.");
+        }
+    }
+
+    @Override
+    public String getTipoReserva() {
+        return "Clase";
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("Reserva Clase - ID: %s, Aula: %s (%s), Fecha: %s, Horario: %s a %s, Responsable: %s, Estado: %s",
+                id, aula.getNombre(), aula.getTipo(), fecha, horaInicio, horaFin, responsable, estado);
+    }
+    
+    public String toCsv() {
+        return getTipoReserva() + "|" + toCsvBase();
+    }
 }
