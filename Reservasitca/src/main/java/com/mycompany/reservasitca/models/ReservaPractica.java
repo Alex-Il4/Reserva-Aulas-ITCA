d1404359cef53f7f99a230346a6101c02510bef0
Alex Jimenez <alexjimenez.az46@gmail.com>
2025-10-19 12:32:43 -0600

    Se implemento herencia de Reserva en ReservaPractica tambien se aplico otra de las reglas para laboratorio

--- Reservasitca/src/main/java/com/mycompany/reservasitca/models/ReservaPractica.java
+++ Reservasitca/src/main/java/com/mycompany/reservasitca/models/ReservaPractica.java
@@ -3,11 +3,46 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.reservasitca.models;
+import exceptions.ReglaReservaException;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 /**
  *
  * @author alex
  */
-public class ReservaPractica {
+
+// Implementa herencia de reserva
+public class ReservaPractica extends Reserva implements Validable {
+
+    public ReservaPractica(String id, Aula aula, LocalDate fecha, LocalTime horaInicio, LocalTime horaFin, String responsable) {
+        super(id, aula, fecha, horaInicio, horaFin, responsable);
+    }
     
+    public ReservaPractica(String id, Aula aula, LocalDate fecha, LocalTime horaInicio, LocalTime horaFin, String responsable, EstadoReserva estado) {
+        super(id, aula, fecha, horaInicio, horaFin, responsable, estado);
+    }
+
+    @Override
+    public void validar() throws ReglaReservaException {
+        // Regla específica: solo en aulas LABORATORIO
+        if (this.getAula().getTipo() != TipoAula.LABORATORIO) {
+            throw new ReglaReservaException("Una Reserva de Práctica solo puede realizarse en aulas LABORATORIO.");
+        }
+    }
+
+    @Override
+    public String getTipoReserva() {
+        return "Práctica";
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("Reserva Práctica - ID: %s, Aula: %s (%s), Fecha: %s, Horario: %s a %s, Responsable: %s, Estado: %s",
+                id, aula.getNombre(), aula.getTipo(), fecha, horaInicio, horaFin, responsable, estado);
+    }
+
+    public String toCsv() {
+        return getTipoReserva() + "|" + toCsvBase();
+    }
 }
